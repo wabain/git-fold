f701702d640f6c69563ec943b6b7d00ff6646e17
William Bain <bain.william.a@gmail.com>
2019-06-01 13:11:26 -0400

    Refactor (blame): Reword parsing

--- git_entropy/blame.py
+++ git_entropy/blame.py
@@ -19,41 +19,11 @@ def run_blame(indexed_range):
 def parse_blame(src_range, blame_lines):
     range_mapping = []
 
-    rev = old_line = new_line = starts_seq = filename = None
-
-    for line in blame_lines:
-        emit = False
-        parts = line.split()
-
-        leader_entry = as_leader(parts)
-        if leader_entry:
-            rev, old_line, new_line, starts_seq = leader_entry
-            if starts_seq:
-                filename = None
-            else:
-                if filename is None:
-                    raise ValueError('missing filename')
-
-                emit = True
-
-        fname_entry = as_filename(parts)
-        if fname_entry:
-            if filename is not None:
-                raise ValueError('multiple filenames specified')
-
-            if rev is None:
-                raise ValueError('filename specified before rev')
-
-            filename = fname_entry
-            emit = True
-
-        if not emit:
-            continue
-
+    for rev, fname, old_line, new_line, starts_seq in get_blame_transforms(blame_lines):
         if range_mapping and not starts_seq:
             last_old, last_new = range_mapping[-1]
             if (
-                last_old.file == filename
+                last_old.file == fname
                 and last_old.start + last_old.extent == old_line
                 and last_new.start + last_new.extent == new_line
             ):
@@ -63,7 +33,7 @@ def parse_blame(src_range, blame_lines):
 
         range_mapping.append(
             (
-                IndexedRange(rev=rev, file=filename, start=old_line, extent=1),
+                IndexedRange(rev=rev, file=fname, start=old_line, extent=1),
                 IndexedRange(
                     rev=src_range.rev, file=src_range.file, start=new_line, extent=1
                 ),
@@ -73,7 +43,55 @@ def parse_blame(src_range, blame_lines):
     return range_mapping
 
 
-def as_leader(parts):
+def get_blame_transforms(blame_lines):
+    """Yield a tuple for each blamed line indicating its source
+
+    For our purposes, we interpret the porcelain blame format as follows:
+
+      <header line (start sequence)>
+      <filename>?
+      <header line (!start sequence)>*
+
+    The filename can be omitted iff there has been a previous entry for the
+    given source commit.
+    """
+    rev = old_line = new_line = starts_seq = filename = None
+
+    # Name of the file for lines originated in a particular commit
+    origin_fnames = {}
+
+    for line in blame_lines:
+        emit = False
+        parts = line.split()
+
+        header_entry = as_header(parts)
+        if header_entry:
+            rev, old_line, new_line, starts_seq = header_entry
+            if starts_seq:
+                filename = origin_fnames.get(rev)
+                emit = filename is not None
+            else:
+                if filename is None:
+                    raise ValueError(f'missing filename for blamed commit {rev}')
+                emit = True
+        else:
+            fname_entry = as_filename(parts)
+            if fname_entry:
+                if filename is not None:
+                    raise ValueError('multiple filenames specified')
+
+                if rev is None:
+                    raise ValueError('filename specified before rev')
+
+                filename = fname_entry
+                origin_fnames[rev] = filename
+                emit = True
+
+        if emit:
+            yield rev, filename, old_line, new_line, starts_seq
+
+
+def as_header(parts):
     """Try to parse blame line into a tuple (oid, old_lineno, new_lineno, starts_seq)"""
     # Line in format <HASH> <OLD-LINENO> <NEW-LINENO> [COUNT]
     if not (
